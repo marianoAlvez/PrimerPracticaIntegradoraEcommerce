861cfb6b97f432f756fa89345b6d88e5dd9aa7d6
Mariano Alvez <alvez_jmariano@hotmail.com>
2024-03-13 08:46:04 -0300

    Agrego cadena de coneccion

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const app = express();
+const mongoose = require('mongoose');
 const productRouter = require('./routes/products.route');
 const cartRouter = require('./routes/carts.route');
 const multer = require('./utils');
 
-//Luego pasar a .env
-const port = 8080;
+//Declarando Express para usar sus funciones.
+const app = express();
 
 //Middlewares
 app.use(express.urlencoded({ extended: true }));
@@ -15,7 +15,21 @@ app.use(express.json());
 app.use('/api/products', productRouter);
 app.use('/api/carts', cartRouter);
 
-//Listen
+
+
+const port = 8080; //todo pasar a .env
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
+
+const connectMongoDB = async ()=>{
+    const stringConnection = 'mongodb://localhost:27017/ecommerce?retryWrites=true&w=majority' //todo pasar a .env
+    try {
+        await mongoose.connect(stringConnection);
+        console.log("Conectado con exito a MongoDB usando Moongose.");
+    } catch (error) {
+        console.error("No se pudo conectar a la BD usando Moongose: " + error);
+        process.exit();
+    }
+};
+connectMongoDB();
